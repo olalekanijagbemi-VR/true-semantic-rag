6361c8a79b74f34a9836b2b1ed51ea3a8475f081
iLakeC#  vr developer <ilakecvrdeveloper@admins-MacBook-Pro.local>
2026-06-15 16:53:48 +0100

    Simplify requirements and fix import

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import tempfile
